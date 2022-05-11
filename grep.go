7f0096cdd69aade5a28693a5b1f41cc61cc8e057
tanqiangyes <826285820@qq.com>
2022-05-11 21:12:20 +0800

    a init

--- grep.go
+++ grep.go
@@ -1,3 +1,8 @@
+// Package main
+// @Author: tanqiangyes
+// @Emainl: 826285820@qq.com
+// @Date: 2022-05-11 21:09:05
+// @Description: a grep written in go. just for study linux and go.
 package main
 
 import (
@@ -6,12 +11,30 @@ import (
 	"os"
 )
 
+// Version : a version for command
+const Version = "0.0.1"
+
 func main() {
 	app := &cli.App{
 		Name:                 "lotus-miner",
 		Usage:                "Filecoin decentralized storage network miner",
-		Version:              "0.0.1",
+		Version:              Version,
 		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "",
+				Aliases:     nil,
+				Usage:       "",
+				EnvVars:     nil,
+				FilePath:    "",
+				Required:    false,
+				Hidden:      false,
+				Value:       false,
+				DefaultText: "",
+				Destination: nil,
+				HasBeenSet:  false,
+			},
+		},
 		Action: func(c *cli.Context) error {
 			//todo
 			return nil
